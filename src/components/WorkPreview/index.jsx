09167c1d5a2c4597af6b2b5e6b898338be7e34df
Cameron Blackwood <cameronjwblackwood@gmail.com>
2022-09-10 17:24:13 +0100

    added correct css

--- src/components/WorkPreview/index.jsx
+++ src/components/WorkPreview/index.jsx
@@ -8,9 +8,12 @@ export default function WorkPreview() {
         src="https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
         alt=""
       />
+      <div className={Styles.titleOver}>
+        <h2>My Appearances</h2>
+      </div>
       <div className={Styles.overlay}>
         <h2>Find Out More!</h2>
-        <p>My comprehensive work history</p>
+        <p>My comprehensive work history as a software engineer.</p>
       </div>
     </a>
   );
