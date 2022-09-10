a2995c0468da6ff946991626b6a45eda8542d77d
Cameron Blackwood <cameronjwblackwood@gmail.com>
2022-09-10 17:23:06 +0100

    fixed overlay

--- src/components/WorkPreview/index.jsx
+++ src/components/WorkPreview/index.jsx
@@ -3,7 +3,7 @@ import Styles from "./styles.module.scss";
 
 export default function WorkPreview() {
   return (
-    <div className={Styles.bg}>
+    <a className={Styles.bg} href="/work">
       <img
         src="https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
         alt=""
@@ -12,6 +12,6 @@ export default function WorkPreview() {
         <h2>Find Out More!</h2>
         <p>My comprehensive work history</p>
       </div>
-    </div>
+    </a>
   );
 }
