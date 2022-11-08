f53ddfc876792ce342c137837bf3218dd28e777a
Cameron Blackwood <cameronjwblackwood@gmail.com>
2022-11-08 18:27:42 +0000

    v1 of the links

--- src/components/ContentLink/index.jsx
+++ src/components/ContentLink/index.jsx
@@ -1,20 +1,45 @@
 import React from "react";
-import "./styles.css";
+import Styles from "./styles.module.scss";
 function ContentLink() {
   const links = [
-    { text: "Twitter", url: "https://www.twitter.com/camblackwood95" },
-    { text: "TikTok", url: "https://www.tiktok.com/@cameronblackwoodcode" },
-    { text: "Podcast", url: "https://www.youtube.com/c/thecodercareer" },
-    { text: "GitHub", url: "https://www.github.com/reikon95" },
-    { text: "LinkedIn", url: "https://www.linkedin.com/in/cameron-blackwood/" },
+    {
+      text: "Twitter",
+      url: "https://www.twitter.com/camblackwood95",
+      description: "Tepid Takes on Tech",
+    },
+    {
+      text: "TikTok",
+      url: "https://www.tiktok.com/@cameronblackwoodcode",
+      description: "Quick bites of useful info!",
+    },
+    {
+      text: "Podcast",
+      url: "https://www.youtube.com/c/thecodercareer",
+      description:
+        "The Coder Career - a podcast about getting the most out of your tech career.",
+    },
+    {
+      text: "GitHub",
+      url: "https://www.github.com/reikon95",
+      description: "My Repos... look at your on risk!",
+    },
+    {
+      text: "LinkedIn",
+      url: "https://www.linkedin.com/in/cameron-blackwood/",
+      description:
+        "Follow me on LinkedIn - or connect if you want to reach me!",
+    },
   ];
   return (
-    <div className="links-wrapper">
+    <div className={Styles.linksWrapper}>
       {links.map((link) => {
         return (
-          <a href={link.url} target="_blank">
-            <button className="btn">{link.text}</button>
-          </a>
+          <div className={Styles.linkWrapper}>
+            <a href={link.url} target="_blank">
+              <button className={Styles.btn}>{link.text}</button>
+            </a>
+            <span>{link.description}</span>
+          </div>
         );
       })}
     </div>
